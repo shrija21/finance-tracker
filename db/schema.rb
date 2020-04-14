d29773a05dd4e57965dcb6f3caae5c1518cd9b02
shri <shrija.tumma1994@gmail.com>
2020-04-14 13:08:05 +0530

    Modify user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_13_061939) do
+ActiveRecord::Schema.define(version: 2020_04_14_070229) do
 
   create_table "stocks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "ticker"
@@ -37,6 +37,8 @@ ActiveRecord::Schema.define(version: 2020_04_13_061939) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
